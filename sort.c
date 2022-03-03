cd89dc24e86d0f644f705afd3d8f94703c8e2ddb
estrong <79113244+Kham02@users.noreply.github.com>
2022-03-03 12:16:33 +0300

    h

--- sort.c
+++ sort.c
@@ -1,1 +1,16 @@
 #include "push_swap.h"
+
+void	qs(t_data *data)
+{
+	data->mid = data->size_a / 2;
+	data->i = 0;
+	while (data->i <= data->size_a)
+	{
+		if (data->a_stack->val <= data->mid)
+			PUSH_B;
+		else
+			ROTATE_A;
+		data->i++;
+	}
+	
+}
