1ec804258969b4fc55d01f51bd2b24573756d6e2
estrong <79113244+Kham02@users.noreply.github.com>
2022-03-10 22:13:24 +0300

    F

--- sort_min.c
+++ sort_min.c
@@ -1,21 +1,5 @@
 #include "push_swap.h"
 
-// void	insertion_sort(t_data *data)
-// {
-// 	data->i = 0;
-// 	data->j = 0
-// 	data->size_a = ft_lstsize(data->a_stack);
-// 	while (data->i <= data->size_a)
-// 	{
-// 		data->j = data->i;
-// 		while (data->j > 0 && data->a_stack->val > )
-// 		{
-// 			//
-// 		}
-// 	}
-// 	check_sort(t_data *data);
-// }
-
 void	sort_two(t_data *data)
 {
 	if (data->a_stack->val > data->a_stack->next->val)
